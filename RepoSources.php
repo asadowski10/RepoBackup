f3fd82c27a3cb20cc3add9e0b51f1a12705fdb35
Alexandre Sadowski <asadowski@beapi.fr>
2016-07-21 15:40:49 +0200

    add Bitbucket API V2 oauth

--- RepoSources.php
+++ RepoSources.php
@@ -1,57 +1,105 @@
 <?php
-interface RepoSource{
-  function setCredentials($params);
-  function getRepositories();
+
+interface RepoSource {
+	function setCredentials( $params );
+
+	function getRepositories();
 }
 
-class BitbucketSource implements RepoSource{
+class BitbucketSource implements RepoSource {
 
-  function setCredentials($params){
-    $this->user = strtolower($params['user']);
-    $this->pass = $params['pass'];
-    $this->client = new Bitbucket\API\Repositories();
-    $this->client->setCredentials( new Bitbucket\API\Authentication\Basic($params['user'], $params['pass']));
-  }
+	function setCredentials( $params ) {
+		$this->user   = strtolower( $params['user'] );
+		$this->pass   = $params['pass'];
+		$this->client = new Bitbucket\API\Repositories();
+		$this->client->setCredentials( new Bitbucket\API\Authentication\Basic( $params['user'], $params['pass'] ) );
+	}
 
-  function getRepositories(){
-      $res = $this->client->all($this->user);
-$list = json_decode($res->getContent(),true);
-$repos = $list['values'];
-while(!empty($list['next'])){
-  $list = $this->client->getClient()->setApiVersion('2.0')->request($list['next']);
-  $list = $list = json_decode($list->getContent(),true);
-  $repos = array_merge($repos,$list['values']);
+	function getRepositories() {
+		$res   = $this->client->all( $this->user );
+		$list  = json_decode( $res->getContent(), true );
+		$repos = $list['values'];
+		while ( ! empty( $list['next'] ) ) {
+			$list  = $this->client->getClient()->setApiVersion( '2.0' )->request( $list['next'] );
+			$list  = $list = json_decode( $list->getContent(), true );
+			$repos = array_merge( $repos, $list['values'] );
+		}
+		$ret = array();
+		foreach ( $repos as $repo ) {
+			$url   = $repo['links']['clone'][1]['href'];
+			$ret[] = array( 'name' => $repo['name'], 'clone_url' => $url, 'type' => $repo['scm'] );
+		}
+
+		return $ret;
+	}
 }
-$ret = array();
-foreach($repos as $repo){
-  $url = $repo['links']['clone'][1]['href'];
-  $ret[] = array('name'=>$repo['name'], 'clone_url' => $url,'type'=>$repo['scm']);
+
+class BitbucketSourceV2 implements RepoSource {
+
+	public $repositories;
+	public $client;
+	public $team;
+
+	function setCredentials( $params ) {
+		$this->client_id     = $params['client_id'];
+		$this->client_secret = $params['client_secret'];
+		$this->team          = $params['team'];
+
+		$oauth_params = array(
+			'client_id'         => $params['client_id'],
+			'client_secret'     => $params['client_secret'],
+		);
+		$this->client = new \Bitbucket\API\Api();
+		$this->client->getClient()->addListener(
+			new \Bitbucket\API\Http\Listener\OAuth2Listener($oauth_params)
+		);
+		/* @var \Bitbucket\API\Repositories $repositories */
+		$this->repositories = $this->client->api('Repositories');
+	}
+
+	function getRepositories() {
+		$res   = $this->repositories->all( $this->team );
+		$list  = json_decode( $res->getContent(), true );
+		$repos = $list['values'];
+
+		while ( ! empty( $list['next'] ) ) {
+			$list  = $this->client->getClient()->setApiVersion( '2.0' )->request( $list['next'] );
+			$list  = $list = json_decode( $list->getContent(), true );
+			$repos = array_merge( $repos, $list['values'] );
+		}
+
+		$ret = array();
+		foreach ( $repos as $repo ) {
+			$url   = $repo['links']['clone'][1]['href'];
+			$ret[] = array( 'name' => $repo['name'], 'clone_url' => $url, 'type' => $repo['scm'] );
+		}
+
+		return $ret;
+	}
 }
 
-return $ret;
-  }
-}
+class GitHubSource implements RepoSource {
 
-class GitHubSource implements RepoSource{
+	function setCredentials( $params ) {
+		$this->user   = $params['user'];
+		$this->client = new \Github\Client(
+			new \Github\HttpClient\CachedHttpClient( array( 'cache_dir' => '/tmp/github-api-cache' ) )
+		);
+		$this->client->authenticate( $params['token'], Github\Client::AUTH_HTTP_TOKEN );
+	}
 
-  function setCredentials($params){
-    $this->user = $params['user'];
-    $this->client = new \Github\Client(
-        new \Github\HttpClient\CachedHttpClient(array('cache_dir' => '/tmp/github-api-cache'))
-    );
-    $this->client->authenticate($params['token'], Github\Client::AUTH_HTTP_TOKEN);
-  }
-  function getRepositories(){
-  $repos = $this->client->api('user')->repositories($this->user);
-  $ret = array();
-  foreach($repos as $repo){
-    $ret[] = array('clone_url' => $repo['clone_url'],'name'=>$repo['name'],'type'=>'git');
-  }
+	function getRepositories() {
+		$repos = $this->client->api( 'user' )->repositories( $this->user );
+		$ret   = array();
+		foreach ( $repos as $repo ) {
+			$ret[] = array( 'clone_url' => $repo['clone_url'], 'name' => $repo['name'], 'type' => 'git' );
+		}
 
-  $gists = $this->client->api('gists')->all();
-  foreach($gists as $gist){
-    $ret[] = array('clone_url' => $gist['git_pull_url'],'name'=>'gists/'.$gist['id'],'type'=>'git');
-  }
-  return $ret;
-  }
+		$gists = $this->client->api( 'gists' )->all();
+		foreach ( $gists as $gist ) {
+			$ret[] = array( 'clone_url' => $gist['git_pull_url'], 'name' => 'gists/' . $gist['id'], 'type' => 'git' );
+		}
+
+		return $ret;
+	}
 }
--- Repobackup.php
+++ Repobackup.php
@@ -5,75 +5,87 @@ require_once 'RepoSources.php';
 $repoBackup = new RepoBackup();
 $repoBackup->run();
 
-class RepoBackup{
-  private $repositories = null;
-  private $gists = null;
-  private $gistsFolder = "gists/";
-  private $repositoriesFolder = "repositories/";
+class RepoBackup {
+	private $repositories = null;
+	private $gists = null;
+	private $gistsFolder = "gists/";
+	private $repositoriesFolder = "repositories/";
 
-  public function __construct(){
-    $this->git = new PHPGit\Git();
-    $this->cwd = getcwd()."/Backups/";
-  }
+	public function __construct() {
+		$this->git = new PHPGit\Git();
+		$this->cwd = getcwd() . "/Backups/";
+	}
 
-  function getInput($message){
-    echo $message;
-    return trim(fgets(STDIN));
-  }
-function run(){
-  echo "Backing up\n";
+	function getInput( $message ) {
+		echo $message;
 
-  $line = $this->getInput("Backup GitHub [y/n]:");
-  if(strtolower($line) == 'y'){
-    $gitSource = new GitHubSource();
-    $user = $this->getInput("GitHub Username:");
-    $token = $this->getInput("GitHub API Token:");
-    $gitSource->setCredentials(array('user'=>$user,'token'=>$token));
-    $this->backupRepositories($gitSource,'GitHub/'.$user."/");
-  }
-  $line = $this->getInput("Backup BitBucket [y/n]:");
-  if(strtolower($line) == 'y'){
-    $user = $this->getInput("BitBucket Username:");
-    $pass = $this->getInput("BitBucket Pass:");
-    $bbSource = new BitbucketSource();
-    $bbSource->setCredentials(array('user'=>$user,'pass'=>$pass));
-    $this->backupRepositories($bbSource,'BitBucket/'.$user.'/');
-  }
-}
-  function backupRepositories(RepoSource $src, $path){
-    $repositories = $src->getRepositories();
-    foreach($repositories as $repository){
-      echo "Pulling ".$repository['name']."\n";
-      $this->backupRepo($repository,$path);
-    }
+		return trim( fgets( STDIN ) );
+	}
 
-  }
+	function run() {
+		echo "Backing up\n";
 
-  function backupRepo($repository,$path){
-    $repoName = $repository['name'];
-    $cloneURL = $repository['clone_url'];
-    $folder = $this->cwd."/".$path.$repoName;
-    echo "Backup Repo: $cloneURL $folder\n";
-    if($repository['type'] == 'git'){
-      if(!file_exists($folder . DIRECTORY_SEPARATOR . '.git')){
-        echo "Calling $cloneURL\n";
-         $this->git->clone($cloneURL, $folder);
-      }
-       $this->git->setRepository($folder);
-       $this->git->fetch->all();
-    }elseif($repository['type'] == 'hg'){
-      $output = array();
-      $return = 0;
-      if(!file_exists($folder . DIRECTORY_SEPARATOR . '.hg')){
-        echo "Calling "."hg clone ".$cloneURL. " ".$folder."\n";
-        exec ("hg clone ".$cloneURL. " ".$folder, $output , $return);
-      }
-      echo "Calling "."hg pull -R ".$folder."\n";
-      exec ("hg pull -R ".$folder, $output , $return);
-    }else{
-      echo "Unrecognised VC type!\n";
-    }
+		$line = $this->getInput( "Backup GitHub [y/n]:" );
+		if ( strtolower( $line ) == 'y' ) {
+			$gitSource = new GitHubSource();
+			$user      = $this->getInput( "GitHub Username:" );
+			$token     = $this->getInput( "GitHub API Token:" );
+			$gitSource->setCredentials( array( 'user' => $user, 'token' => $token ) );
+			$this->backupRepositories( $gitSource, 'GitHub/' . $user . "/" );
+		}
+		$line = $this->getInput( "Backup BitBucket [y/n]:" );
+		if ( strtolower( $line ) == 'y' ) {
+			$user     = $this->getInput( "BitBucket Username:" );
+			$pass     = $this->getInput( "BitBucket Pass:" );
+			$bbSource = new BitbucketSource();
+			$bbSource->setCredentials( array( 'user' => $user, 'pass' => $pass ) );
+			$this->backupRepositories( $bbSource, 'BitBucket/' . $user . '/' );
+		}
+		$line = $this->getInput( "Backup BitBucket v2 [y/n]:" );
+		if ( strtolower( $line ) == 'y' ) {
+			$user     = $this->getInput( "BitBucket client_id:" );
+			$pass     = $this->getInput( "BitBucket client_secret:" );
+			$team     = $this->getInput( "BitBucket Team:" );
+			$bbSource = new BitbucketSourceV2();
+			$bbSource->setCredentials( array( 'client_id' => $user, 'client_secret' => $pass, 'team' => $team ) );
+			$this->backupRepositories( $bbSource, 'BitBucket/' . $user . '/' );
+		}
+	}
 
-  }
+	function backupRepositories( RepoSource $src, $path ) {
+		$repositories = $src->getRepositories();
+		foreach ( $repositories as $repository ) {
+			echo "Pulling " . $repository['name'] . "\n";
+			$this->backupRepo( $repository, $path );
+		}
+
+	}
+
+	function backupRepo( $repository, $path ) {
+		$repoName = $repository['name'];
+		$cloneURL = $repository['clone_url'];
+		$folder   = $this->cwd . "/" . $path . $repoName;
+		echo "Backup Repo: $cloneURL $folder\n";
+		if ( $repository['type'] == 'git' ) {
+			if ( ! file_exists( $folder . DIRECTORY_SEPARATOR . '.git' ) ) {
+				echo "Calling $cloneURL\n";
+				$this->git->clone( $cloneURL, $folder );
+			}
+			$this->git->setRepository( $folder );
+			$this->git->fetch->all();
+		} elseif ( $repository['type'] == 'hg' ) {
+			$output = array();
+			$return = 0;
+			if ( ! file_exists( $folder . DIRECTORY_SEPARATOR . '.hg' ) ) {
+				echo "Calling " . "hg clone " . $cloneURL . " " . $folder . "\n";
+				exec( "hg clone " . $cloneURL . " " . $folder, $output, $return );
+			}
+			echo "Calling " . "hg pull -R " . $folder . "\n";
+			exec( "hg pull -R " . $folder, $output, $return );
+		} else {
+			echo "Unrecognised VC type!\n";
+		}
+
+	}
 
 }
